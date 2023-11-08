dc489fc8106a047339c6d8c29def2a73cafc3b62
Casper Hooft <Casper.Hooft@ordina.nl>
2023-11-08 15:16:23 +0100

    Add project files.

---
+++ UI Testing/WrittenTestCase.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using Microsoft.VisualBasic;
+using NUnit.Framework;
+
+namespace UITests;
+
+[Parallelizable(ParallelScope.Self)]
+
+[TestFixture]
+public class NUnitPlaywright : PageTest
+{
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync(url: "http://www.eaapp.somee.com");
+    }
+
+    [Test]
+    public async Task LoginUsingValidCredentials()
+    {        
+        await Page.ClickAsync(selector: "text=Login");
+
+/*        await Page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "loginScreen.jpg"
+        });
+*/
+        await Page.FillAsync(selector: "#UserName", value: "admin");
+        await Page.FillAsync(selector: "#Password", value: "password");
+        await Page.ClickAsync(selector: "text=Log in");
+        await Expect(Page.Locator(selector: "text='Employee Details'")).ToBeVisibleAsync();
+
+/*        await Page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "Hello Admin.jpg"
+        });
+*/
+    }
+
+}
